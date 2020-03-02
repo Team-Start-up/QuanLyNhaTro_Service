947f7689ebaa7449630b07bb3e2f306c8e197428
thiennguyen5200 <thiennguyen5200@gmail.com>
2020-03-02 15:04:08 +0700

    Update motel.service.js

--- services/motel.service.js
+++ services/motel.service.js
@@ -4,8 +4,9 @@ const { RoomModel } = require('../models/room.model')
 class MotelService {
     static async create_motel(author, motel_name, description, address, county, city, district, qty_room) {
         const auth = UserModel.findOne({_id: author})
-        if(!auth) throw new Error ('Tai khoan cua ban khong ton tai')
-        const motel = MotelModel.create({
+        console.log('auth: '+ (await auth))
+        if(!(await auth)) throw new Error ('Tai khoan cua ban khong ton tai')
+        return MotelModel.create({
             author,
             motel_name,
             description,
@@ -15,33 +16,34 @@ class MotelService {
             district,
             qty_room,
             status: 1
-        })
-        if(!motel) throw new Error ('Loi! Khong the tao thong tin nha tro.')
-        for (let index = 0; index < motel.qty_room; index++) {
-            const roomInsert = await RoomModel.create(
-                {
-                    name:`Room ${index + 1}`,
-                    status: true,
-                    enter_date: Date.now(),
-                    motel: motel._id
-                }
-            )
-            const motelUpdate = await MotelModel.findByIdAndUpdate(
-                {_id:motel._id},
-                {
-                    $addToSet:{rooms:roomInsert._id}
-                },
-                {new : true}
-            )
-        }
-        
-        const userUpdate = await UserModel.findByIdAndUpdate(
-            {_id:author},
-            {
-                $addToSet: {motels: motel._id}
+        }).then(async motel => {
+            for (let index = 0; index < motel.qty_room; index++) {
+                await RoomModel.create(
+                    {
+                        name:`Room ${index + 1}`,
+                        status: true,
+                        enter_date: Date.now(),
+                        motel: motel._id
+                    }
+                ).then(async result => {
+                    const t = await MotelModel.findByIdAndUpdate(
+                        {_id:motel._id},
+                        {
+                            $addToSet:{rooms:result._id}
+                        },
+                        {new : true}
+                    )
+                })
             }
-        ).select('email name')
-        return {motel,userUpdate}
+            const userUpdate = await UserModel.findByIdAndUpdate(
+                {_id:author},
+                {
+                    $addToSet: {motels: motel._id}
+                },
+                {new: true}
+            ).select('email name')
+            return {motel,userUpdate}
+        })
     }
 }
 
