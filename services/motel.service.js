858808338caefa2cf3465eef08151200faa1c3b0
Thiện Nguyễn <thiennguyen5200@gmail.com>
2020-03-21 08:21:09 +0700

    Update motel.service.js

--- services/motel.service.js
+++ services/motel.service.js
@@ -5,7 +5,7 @@ class MotelService {
     static async create_motel(author, motel_name, description, address, county, city, district, qty_room) {
         const auth = UserModel.findOne({_id: author})
         if(!auth) throw new Error ('Tai khoan cua ban khong ton tai')
-        const motel = MotelModel.create({
+        await MotelModel.create({
             author,
             motel_name,
             description,
@@ -15,24 +15,36 @@ class MotelService {
             district,
             qty_room,
             status: 1
-        })
+        }).then(result => result)
+        console.log('motel: ' + motel);
         if(!motel) throw new Error ('Loi! Khong the tao thong tin nha tro.')
         for (let index = 0; index < motel.qty_room; index++) {
-            const roomInsert = await RoomModel.create(
+            console.log(index)
+            await RoomModel.create(
                 {
                     name:`Room ${index + 1}`,
                     status: true,
                     enter_date: Date.now(),
                     motel: motel._id
                 }
-            )
-            const motelUpdate = await MotelModel.findByIdAndUpdate(
-                {_id:motel._id},
-                {
-                    $addToSet:{rooms:roomInsert._id}
-                },
-                {new : true}
-            )
+            ).then(async result => {
+                await MotelModel.findByIdAndUpdate(
+                    {_id:motel._id},
+                    {
+                        $addToSet:{rooms:result._id}
+                    },
+                    {new : true}
+                )
+            })
+            // console.log(roomInsert);
+            // const motelUpdate = await MotelModel.findByIdAndUpdate(
+            //     {_id:motel._id},
+            //     {
+            //         $addToSet:{rooms:roomInsert._id}
+            //     },
+            //     {new : true}
+            // )
+            // console.log(motelUpdate);
         }
         
         const userUpdate = await UserModel.findByIdAndUpdate(
@@ -41,6 +53,7 @@ class MotelService {
                 $addToSet: {motels: motel._id}
             }
         ).select('email name')
+        console.log(userUpdate)
         return {motel,userUpdate}
     }
 }
